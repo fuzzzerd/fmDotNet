55f5c022e81739515dc5d72250cfd4ccc8f7d435
Nate Bross <nate@brosstribe.com>
2013-06-12 15:48:56 -0500

    New test data file, more tests
    Added another CompoundFind test using AND operation.

--- src/fmDotNet.Tests/CompoundFindTests.cs
+++ src/fmDotNet.Tests/CompoundFindTests.cs
@@ -50,7 +50,36 @@ namespace fmDotNet.Tests
             // assert
             Assert.IsTrue(blueCount >= 1, "Must have one or more Blues.");
             Assert.IsTrue(redCount >= 1, "Must have one or more Reds.");
-            Assert.AreEqual(blueCount+redCount, response.Tables[0].Rows.Count);
+            Assert.AreEqual(blueCount + redCount, response.Tables[0].Rows.Count);
+        }
+
+
+        [TestMethod]
+        public void CompoundFind_Red_AND_Glass_ReturnsRedAndGlass()
+        {
+            // arrange 
+            var fms = this.SetupFMSAxml();
+            fms.SetDatabase("fmDotNet.Tests", false);
+            fms.SetLayout("FindRequest.Tests");
+            // find how many red and how many blue we have
+
+            // act
+            var cpfRequest = fms.CreateCompoundFindRequest();
+            cpfRequest.AddSearchCriterion("CupTypes::Type", "Glass", false, false);
+            cpfRequest.AddSearchCriterion("Colors::Name", "Red", false, false);
+            var response = cpfRequest.Execute();
+
+            var countCorrect = 0;
+            foreach (DataRow dr in response.Tables[0].Rows)
+            {
+                if (dr["Colors::Name"].ToString() == "Red"
+                    && dr["CupTypes::Type"].ToString() == "Glass") { countCorrect++; }
+                
+            }
+
+            // assert
+            Assert.AreNotEqual(0, response.Tables[0].Rows.Count, "No records found, check database data"); // make sure we found at least one
+            Assert.AreEqual(countCorrect, response.Tables[0].Rows.Count);
         }
     }
 }
